3ef6fff7388d5cde79dfd917583776199644ddc3
Yana Peshko <yanapeshko917@gmail.com>
2023-03-09 18:37:43 -0700

    setup global styles

---
+++ src/components/Header.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { NavLink, Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <>
+      <header className="header-top-strip py-3">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-6">
+              <p className="text-white mb-0">
+                Free Shipping Over $100 & Free Returns
+              </p>
+            </div>
+            <div className="col-6">
+              <p className="text-end text-white mb-0">
+                Hotline:
+                <a className="text-white" href="tel:+91 8264954234">
+                  +91 8264954234
+                </a>
+              </p>
+            </div>
+          </div>
+        </div>
+      </header>
+    </>
+  );
+};
+
+export default Header;
