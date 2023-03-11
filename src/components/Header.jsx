9d28fbe3b21529ebbc00e6a0fa18a7be7c1901d8
Yana Peshko <yanapeshko917@gmail.com>
2023-03-10 19:15:16 -0700

    added Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { NavLink, Link } from "react-router-dom";
+import { BsSearch } from "react-icons/bs";
 
 const Header = () => {
   return (
@@ -22,6 +23,32 @@ const Header = () => {
             </div>
           </div>
         </div>
+        <header className="headre-upper py-3">
+          <div className="container-xxl">
+            <div className="row">
+              <div className="col-2">
+                <h2>
+                  <Link className="text-white">Dev Corner</Link>
+                </h2>
+              </div>
+              <div className="col-5">
+                <div class="input-group mb-3">
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Search Product Here..."
+                    aria-label="Search Product Here..."
+                    aria-describedby="basic-addon2"
+                  />
+                  <span className="input-group-text" id="basic-addon2">
+                    <BsSearch />
+                  </span>
+                </div>
+              </div>
+              <div className="col-5"></div>
+            </div>
+          </div>
+        </header>
       </header>
     </>
   );
