f2a78503447eb1187a6d6fc989f7dc9952faa8f5
Yana Peshko <yanapeshko917@gmail.com>
2023-03-11 20:14:01 -0500

    bottom-header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -23,32 +23,101 @@ const Header = () => {
             </div>
           </div>
         </div>
-        <header className="headre-upper py-3">
-          <div className="container-xxl">
-            <div className="row">
-              <div className="col-2">
-                <h2>
-                  <Link className="text-white">Dev Corner</Link>
-                </h2>
+      </header>
+      <header className="header-upper py-3">
+        <div className="container-xxl">
+          <div className="row align-items-center">
+            <div className="col-2">
+              <h2>
+                <Link className="text-white">Dev Corner</Link>
+              </h2>
+            </div>
+            <div className="col-5">
+              <div className="input-group mb-3">
+                <input
+                  type="text"
+                  className="form-control py-2"
+                  placeholder="Search Product Here..."
+                  aria-label="Search Product Here..."
+                  aria-describedby="basic-addon2"
+                />
+                <span className="input-group-text p-3" id="basic-addon2">
+                  <BsSearch className="fs-6" />
+                </span>
               </div>
-              <div className="col-5">
-                <div class="input-group mb-3">
-                  <input
-                    type="text"
-                    className="form-control"
-                    placeholder="Search Product Here..."
-                    aria-label="Search Product Here..."
-                    aria-describedby="basic-addon2"
-                  />
-                  <span className="input-group-text" id="basic-addon2">
-                    <BsSearch />
-                  </span>
+            </div>
+            <div className="col-5">
+              <div className="header-upper-links d-flex align-items-center justify-content-between">
+                <div>
+                  <Link className="d-flex align-items-center gap-10 text-white">
+                    <img src="images/compare.svg" alt="compare" />
+                    <p className="mb-0">
+                      Compare
+                      <br />
+                      Products
+                    </p>
+                  </Link>
+                </div>
+                <div>
+                  <Link className="d-flex align-items-center gap-10 text-white">
+                    <img src="images/wishlist.svg" alt="wishlist" />
+                    <p className="mb-0">
+                      Favourite
+                      <br />
+                      Wishlist
+                    </p>
+                  </Link>
+                </div>
+                <div>
+                  <Link className="d-flex align-items-center gap-10 text-white">
+                    <img src="images/user.svg" alt="user" />
+                    <p className="mb-0">
+                      Login
+                      <br />
+                      My Account
+                    </p>
+                  </Link>
+                </div>
+                <div>
+                  <Link className="d-flex align-items-center gap-10 text-white">
+                    <img src="images/cart.svg" alt="cart" />
+                    <div className="d-flex flex-column gap-10">
+                      <span className="badge bg-white text-dark">0</span>
+                      <p className="mb-0">$ 500</p>
+                    </div>
+                  </Link>
                 </div>
               </div>
-              <div className="col-5"></div>
             </div>
           </div>
-        </header>
+        </div>
+      </header>
+      <header className="header-bottom py-3">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <div className="menu-bottom d-flex align-items-center">
+                <div></div>
+                <div className="menu-links">
+                  <div className="d-flex align-items-center gap-15">
+                    <NavLink className="text-white" to="/">
+                      Home
+                    </NavLink>
+                    <NavLink className="text-white" to="/">
+                      Our Store
+                    </NavLink>
+                    <NavLink className="text-white" to="/">
+                      Blogs
+                    </NavLink>
+                    <NavLink className="text-white" to="/contact">
+                      Contact
+                    </NavLink>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </header>
     </>
   );
