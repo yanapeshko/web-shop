543689de736c7de7426f0231aed95e08157809d6
Yana Peshko <yanapeshko917@gmail.com>
2023-03-16 22:29:23 -0400

    started footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <>
+      <footer className="py-3">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-5"></div>
+            <div className="col-7"></div>
+          </div>
+        </div>
+      </footer>
+      <footer className="py-3"></footer>
+      <footer className="py-4">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <p className="text-center mb-0 text-white">
+                &copy;{new Date().getFullYear()} Powered by Developer's Corner
+              </p>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
 };
 
 export default Footer;
