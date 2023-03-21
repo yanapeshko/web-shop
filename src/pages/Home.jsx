a16aabcb0e25c3f34262c91e584ea533dd3f73ab
Yana Peshko <yanapeshko917@gmail.com>
2023-03-20 21:39:08 -0400

    started Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <section className="home-wrapper-1 py-5">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-6">
+              <div className="main-banner-conten p-3">
+                <img src="/public/images/main-banner.jpg" alt="" />
+              </div>
+            </div>
+            <div className="col-6"></div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Home;
