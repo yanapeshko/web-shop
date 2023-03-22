7d049492239ff61477788694b13553181fc35ed7
Yana Peshko <yanapeshko917@gmail.com>
2023-03-21 21:40:40 -0400

    main banner

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
@@ -7,11 +8,37 @@ const Home = () => {
         <div className="container-xxl">
           <div className="row">
             <div className="col-6">
-              <div className="main-banner-conten p-3">
-                <img src="/public/images/main-banner.jpg" alt="" />
+              <div className="main-banner position-relative p-3">
+                <img
+                  src="images/main-banner-1.jpg"
+                  className="img-fluid rounded-3"
+                  alt="main banner"
+                />
+                <div className="main-banner-content position-absolute">
+                  <h4>SUPERCHARGED FOR PROS.</h4>
+                  <h5>iPad S13+ Pro.</h5>
+                  <p>From $999.00 or $41.62/mo.</p>
+                  <Link className="button">BUY NOW</Link>
+                </div>
               </div>
             </div>
-            <div className="col-6"></div>
+            <div className="col-6">
+              <div className="d-flex flex-wrap justify-content-between align-items-center">
+                <div className="main-banner position-relative p-3">
+                  <img
+                    src="images/main-banner-1.jpg"
+                    className="img-fluid rounded-3"
+                    alt="main banner"
+                  />
+                  <div className="main-banner-content position-absolute">
+                    <h4>SUPERCHARGED FOR PROS.</h4>
+                    <h5>iPad S13+ Pro.</h5>
+                    <p>From $999.00 or $41.62/mo.</p>
+                    <Link className="button">BUY NOW</Link>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </section>
