945c1e2448bb5641e16fb69db3d55bb5ae5c6621
Yana Peshko <yanapeshko917@gmail.com>
2023-03-22 21:33:35 -0400

    added little banners

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -24,17 +24,52 @@ const Home = () => {
             </div>
             <div className="col-6">
               <div className="d-flex flex-wrap justify-content-between align-items-center">
-                <div className="main-banner position-relative p-3">
+                <div className="small-banner position-relative p-3">
                   <img
-                    src="images/main-banner-1.jpg"
+                    src="images/catbanner-01.jpg"
                     className="img-fluid rounded-3"
                     alt="main banner"
                   />
-                  <div className="main-banner-content position-absolute">
-                    <h4>SUPERCHARGED FOR PROS.</h4>
+                  <div className="small-banner-content position-absolute">
+                    <h4>Best Sale</h4>
                     <h5>iPad S13+ Pro.</h5>
                     <p>From $999.00 or $41.62/mo.</p>
-                    <Link className="button">BUY NOW</Link>
+                  </div>
+                </div>
+                <div className="small-banner position-relative p-3">
+                  <img
+                    src="images/catbanner-02.jpg"
+                    className="img-fluid rounded-3"
+                    alt="main banner"
+                  />
+                  <div className="small-banner-content position-absolute">
+                    <h4>NEW ARRIVAL</h4>
+                    <h5>buy iPad Air</h5>
+                    <p>From $599 or $49.91/mo. for 12 mo.*</p>
+                  </div>
+                </div>
+                <div className="small-banner position-relative p-3">
+                  <img
+                    src="images/catbanner-02.jpg"
+                    className="img-fluid rounded-3"
+                    alt="main banner"
+                  />
+                  <div className="small-banner-content position-absolute">
+                    <h4>NEW ARRIVAL</h4>
+                    <h5>buy iPad Air</h5>
+                    <p>From $599 or $49.91/mo. for 12 mo.*</p>
+                  </div>
+                </div>
+                <div className="small-banner position-relative p-3">
+                  <img
+                    src="images/catbanner-02.jpg"
+                    className="img-fluid rounded-3"
+                    alt="main banner"
+                  />
+                  <div className="small-banner-content position-absolute">
+                    <h4>NEW ARRIVAL</h4>
+                    <h5>buy iPad Air</h5>
+                    <p>From $599 or $49.91/mo. for 12 mo.*</p>
                   </div>
                 </div>
               </div>
