6834b7647aa6ea2bc911c2dcb9f089d9621af0fd
Yana Peshko <yanapeshko917@gmail.com>
2023-03-23 21:23:58 -0400

    added section-2 into Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,7 +8,7 @@ const Home = () => {
         <div className="container-xxl">
           <div className="row">
             <div className="col-6">
-              <div className="main-banner position-relative p-3">
+              <div className="main-banner position-relative">
                 <img
                   src="images/main-banner-1.jpg"
                   className="img-fluid rounded-3"
@@ -23,20 +23,23 @@ const Home = () => {
               </div>
             </div>
             <div className="col-6">
-              <div className="d-flex flex-wrap justify-content-between align-items-center">
-                <div className="small-banner position-relative p-3">
+              <div className="d-flex flex-wrap gap-10 justify-content-between align-items-center">
+                <div className="small-banner position-relative">
                   <img
                     src="images/catbanner-01.jpg"
                     className="img-fluid rounded-3"
                     alt="main banner"
                   />
                   <div className="small-banner-content position-absolute">
-                    <h4>Best Sale</h4>
+                    <h4>BEST SALE</h4>
                     <h5>iPad S13+ Pro.</h5>
-                    <p>From $999.00 or $41.62/mo.</p>
+                    <p>
+                      From $999.00 <br />
+                      or $41.62/mo.
+                    </p>
                   </div>
                 </div>
-                <div className="small-banner position-relative p-3">
+                <div className="small-banner position-relative">
                   <img
                     src="images/catbanner-02.jpg"
                     className="img-fluid rounded-3"
@@ -45,31 +48,85 @@ const Home = () => {
                   <div className="small-banner-content position-absolute">
                     <h4>NEW ARRIVAL</h4>
                     <h5>buy iPad Air</h5>
-                    <p>From $599 or $49.91/mo. for 12 mo.*</p>
+                    <p>
+                      From $599 <br />
+                      or $49.91/mo. for 12 mo.*
+                    </p>
                   </div>
                 </div>
-                <div className="small-banner position-relative p-3">
+                <div className="small-banner position-relative">
                   <img
-                    src="images/catbanner-02.jpg"
+                    src="images/catbanner-03.jpg"
                     className="img-fluid rounded-3"
                     alt="main banner"
                   />
                   <div className="small-banner-content position-absolute">
                     <h4>NEW ARRIVAL</h4>
                     <h5>buy iPad Air</h5>
-                    <p>From $599 or $49.91/mo. for 12 mo.*</p>
+                    <p>
+                      From $599 <br />
+                      or $49.91/mo. for 12 mo.*
+                    </p>
                   </div>
                 </div>
-                <div className="small-banner position-relative p-3">
+                <div className="small-banner position-relative">
                   <img
-                    src="images/catbanner-02.jpg"
+                    src="images/catbanner-04.jpg"
                     className="img-fluid rounded-3"
                     alt="main banner"
                   />
                   <div className="small-banner-content position-absolute">
                     <h4>NEW ARRIVAL</h4>
                     <h5>buy iPad Air</h5>
-                    <p>From $599 or $49.91/mo. for 12 mo.*</p>
+                    <p>
+                      From $599 <br />
+                      or $49.91/mo. for 12 mo.*
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="home-wrapper-2 py-5">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <div className="services d-flex align-items-center justify-content-between">
+                <div className="d-flex align-items-center gap-15">
+                  <img src="images/service.png" alt="services" />
+                  <div>
+                    <h6>Free Shipping</h6>
+                    <p className="mb-0">Frow all orders over $100</p>
+                  </div>
+                </div>
+                <div className="d-flex align-items-center gap-15">
+                  <img src="images/service-02.png" alt="services" />
+                  <div>
+                    <h6>Daily Surprise Offers</h6>
+                    <p className="mb-0">Save up to 25% off</p>
+                  </div>
+                </div>
+                <div className="d-flex align-items-center gap-15">
+                  <img src="images/service-03.png" alt="services" />
+                  <div>
+                    <h6>Support 24/7</h6>
+                    <p className="mb-0">Shop with an experts</p>
+                  </div>
+                </div>
+                <div className="d-flex align-items-center gap-15">
+                  <img src="images/service-04.png" alt="services" />
+                  <div>
+                    <h6>Affordable prices</h6>
+                    <p className="mb-0">Get Factory Default Price</p>
+                  </div>
+                </div>
+                <div className="d-flex align-items-center gap-15">
+                  <img src="images/service-05.png" alt="services" />
+                  <div>
+                    <h6>Secure Payments</h6>
+                    <p className="mb-0">100% Protected Payment</p>
                   </div>
                 </div>
               </div>
