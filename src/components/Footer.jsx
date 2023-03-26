a04abad4c7a4f865136abbacfc6cd806330043be
Yana Peshko <yanapeshko917@gmail.com>
2023-03-26 19:32:54 -0400

    finished marquee section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -56,16 +56,16 @@ const Footer = () => {
                   yanapeshko917@gmail.com
                 </a>
                 <div className="social_icons d-flex align-items-center gap-30 mt-4">
-                  <a href="#" className="text-white">
+                  <a className="text-white" href="#">
                     <BsLinkedin className="fs-4" />
                   </a>
-                  <a href="#" className="text-white">
+                  <a className="text-white" href="#">
                     <BsInstagram className="fs-4" />
                   </a>
-                  <a href="#" className="text-white">
+                  <a className="text-white" href="#">
                     <BsGithub className="fs-4" />
                   </a>
-                  <a href="#" className="text-white">
+                  <a className="text-white" href="#">
                     <BsYoutube className="fs-4" />
                   </a>
                 </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import Marquee from "react-fast-marquee";
 
 const Home = () => {
   return (
@@ -195,6 +196,42 @@ const Home = () => {
                   </div>
                   <img src="images/headphone.jpg" alt="watch" />
                 </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="marquee-wrapper py-5">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <div className="marquee-inner-wrapper card-wrapper">
+                <Marquee className="d-flex">
+                  <div className="mx-4 w-25">
+                    <img src="images/brand-01.png" alt="brand" />
+                  </div>
+                  <div className="mx-4 w-25">
+                    <img src="images/brand-02.png" alt="brand" />
+                  </div>
+                  <div className="mx-4 w-25">
+                    <img src="images/brand-03.png" alt="brand" />
+                  </div>
+                  <div className="mx-4 w-25">
+                    <img src="images/brand-04.png" alt="brand" />
+                  </div>
+                  <div className="mx-4 w-25">
+                    <img src="images/brand-05.png" alt="brand" />
+                  </div>
+                  <div className="mx-4 w-25">
+                    <img src="images/brand-06.png" alt="brand" />
+                  </div>
+                  <div className="mx-4 w-25">
+                    <img src="images/brand-07.png" alt="brand" />
+                  </div>
+                  <div className="mx-4 w-25">
+                    <img src="images/brand-08.png" alt="brand" />
+                  </div>
+                </Marquee>
               </div>
             </div>
           </div>
