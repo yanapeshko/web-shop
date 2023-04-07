10d518346a9eb383be269e3a76fe0da12adba24e
Yana Peshko <yanapeshko917@gmail.com>
2023-04-06 21:22:11 -0400

    famous section is done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -242,9 +242,41 @@ const Home = () => {
                   alt="famous"
                 />
                 <div className="famous-content position-absolute">
-                  <h5>Big Screen</h5>
-                  <h6>Smart Watch Series 7</h6>
-                  <p>From $399or $16.62/mo. for 24 mo. *</p>
+                  <h5 className="text-dark">Studio Display</h5>
+                  <h6 className="text-dark">600 nits of brightness.</h6>
+                  <p className="text-dark">27-inch 5K Retina display</p>
+                </div>
+              </div>
+            </div>
+            <div className="col-3">
+              <div className="famous-card position-relative">
+                <img
+                  className="img-fluid"
+                  src="images/famous-03.webp"
+                  alt="famous"
+                />
+                <div className="famous-content position-absolute">
+                  <h5 className="text-dark">smartphones</h5>
+                  <h6 className="text-dark">Smartphone 13 Pro.</h6>
+                  <p className="text-dark">
+                    Now in Green. From $999.00 or $41.62/mo.for 24 mo. Footnote*
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-3">
+              <div className="famous-card position-relative">
+                <img
+                  className="img-fluid"
+                  src="images/famous-04.webp"
+                  alt="famous"
+                />
+                <div className="famous-content position-absolute">
+                  <h5 className="text-dark">home speakers</h5>
+                  <h6 className="text-dark">Room-filling sound.</h6>
+                  <p className="text-dark">
+                    From $699 or $116.58/mo. for 12 mo.*
+                  </p>
                 </div>
               </div>
             </div>
