54175a5a1fb4ef9c5c4ec761926c41f336997faf
Yana Peshko <yanapeshko917@gmail.com>
2023-04-07 21:54:26 -0400

    OurStore

--- src/pages/OurStore.jsx
+++ src/pages/OurStore.jsx
@@ -10,7 +10,28 @@ const OurStore = () => {
       <div className="store-wrapper home-wrapper py-5">
         <div className="container-xxl">
           <div className="row">
-            <div className="col-3"></div>
+            <div className="col-3">
+              <div className="filter-card mb-3">
+                <h3>Shop By Categories</h3>
+                <div>
+                  <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                  </ul>
+                </div>
+              </div>
+              <div className="filter-card mb-3">
+                <h3>Filter By</h3>
+              </div>
+              <div className="filter-card mb-3">
+                <h3>Product Tags</h3>
+              </div>
+              <div className="filter-card mb-3">
+                <h3>Random Product</h3>
+              </div>
+            </div>
             <div className="col-9"></div>
           </div>
         </div>
