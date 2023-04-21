5d1bef1a14d6fb129dd5cd9c0b5869f139ddee96
Yana Peshko <yanapeshko917@gmail.com>
2023-04-21 18:49:16 -0400

    ContactPage is done

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -72,7 +72,7 @@ const Contact = () => {
                     <ul className="ps-0">
                       <li className="mb-3 d-flex gap-15 align-items-center">
                         <AiOutlineHome className="fs-5" />
-                        <address>
+                        <address className="mb-0">
                           26 Lugano Crescent, Markham, L6E1E3, ON, Canada
                         </address>
                       </li>
