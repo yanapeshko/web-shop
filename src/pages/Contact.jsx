71e0d11d285e4b479734d83dd282741fe68fbb85
Yana Peshko <yanapeshko917@gmail.com>
2023-04-20 20:47:22 -0400

    +form-control

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { AiOutlineHome, AiOutlineMail } from "react-icons/ai";
+import { BiInfoCircle, BiPhoneCall } from "react-icons/bi";
+
 import Meta from "../components/Meta";
 import BreadCrumb from "../components/BreadCrumb";
 
@@ -28,13 +31,25 @@ const Contact = () => {
                   <h3 className="contact-title mb-4">Contact</h3>
                   <form action="" className="d-flex flex-column gap-15">
                     <div>
-                      <input type="text" className="form-control" />
+                      <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Name"
+                      />
                     </div>
                     <div>
-                      <input type="text" className="form-control" />
+                      <input
+                        type="email"
+                        className="form-control"
+                        placeholder="Email"
+                      />
                     </div>
                     <div>
-                      <input type="text" className="form-control" />
+                      <input
+                        type="tel"
+                        className="form-control"
+                        placeholder="Mobile Number"
+                      />
                     </div>
                     <div>
                       <textarea
@@ -42,13 +57,41 @@ const Contact = () => {
                         id=""
                         className="w-100 form-control"
                         cols="30"
-                        rows="10"
+                        rows="4"
+                        placeholder="Comments"
                       ></textarea>
+                    </div>
+                    <div>
+                      <button className="button border-0">Submit</button>
                     </div>
                   </form>
                 </div>
                 <div>
                   <h3 className="contact-title mb-4">Get in touch with us</h3>
+                  <div>
+                    <ul className="ps-0">
+                      <li className="mb-3 d-flex gap-15 align-items-center">
+                        <AiOutlineHome className="fs-5" />
+                        <address>
+                          26 Lugano Crescent, Markham, L6E1E3, ON, Canada
+                        </address>
+                      </li>
+                      <li className="mb-3 d-flex gap-15 align-items-center">
+                        <BiPhoneCall className="fs-5" />
+                        <a href="tel:+1 4373312094">+1 4373312094</a>
+                      </li>
+                      <li className="mb-3 d-flex gap-15 align-items-center">
+                        <AiOutlineMail className="fs-5" />
+                        <a href="mailto:yanapeshko917@gmail.com">
+                          yanapeshko917@gmail.com
+                        </a>
+                      </li>
+                      <li className="mb-3 d-flex gap-15 align-items-center">
+                        <BiInfoCircle className="fs-5" />
+                        <p className="mb-0">Monday-Friday 10 AM - 8 PM</p>
+                      </li>
+                    </ul>
+                  </div>
                 </div>
               </div>
             </div>
