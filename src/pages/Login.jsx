176cdcd9db4d5a2104bd849d24b90107723544f5
Yana Peshko <yanapeshko917@gmail.com>
2023-04-23 13:31:13 -0400

    removed scroll from pages

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -10,37 +10,39 @@ const Login = () => {
       <Meta title={"Login"} />
       <BreadCrumb title="Login" />
       <div className="login-wrapper home-wrapper-2 py-5">
-        <div className="row">
-          <div className="col-12">
-            <div className="auth-card">
-              <h3 className="text-center mb-3">Login</h3>
-              <form className="d-flex flex-column gap-15" action="">
-                <div className="mt-1">
-                  <input
-                    className="form-control"
-                    type="email"
-                    name="email"
-                    placeholder="Email"
-                  />
-                </div>
-                <div className="mt-1">
-                  <input
-                    className="form-control"
-                    type="password"
-                    name="password"
-                    placeholder="Password"
-                  />
-                </div>
-                <div>
-                  <Link to="/forgot-password">Forgot Password?</Link>
-                  <div className=" mt-3 d-flex justify-content-center align-items-center gap-15">
-                    <button className="button border-0">Login</button>
-                    <Link to="/signup" className="button signup">
-                      SignUp
-                    </Link>
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <div className="auth-card">
+                <h3 className="text-center mb-3">Login</h3>
+                <form className="d-flex flex-column gap-15" action="">
+                  <div className="mt-1">
+                    <input
+                      className="form-control"
+                      type="email"
+                      name="email"
+                      placeholder="Email"
+                    />
                   </div>
-                </div>
-              </form>
+                  <div className="mt-1">
+                    <input
+                      className="form-control"
+                      type="password"
+                      name="password"
+                      placeholder="Password"
+                    />
+                  </div>
+                  <div>
+                    <Link to="/forgot-password">Forgot Password?</Link>
+                    <div className=" mt-3 d-flex justify-content-center align-items-center gap-15">
+                      <button className="button border-0">Login</button>
+                      <Link to="/signup" className="button signup">
+                        SignUp
+                      </Link>
+                    </div>
+                  </div>
+                </form>
+              </div>
             </div>
           </div>
         </div>
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -9,49 +9,51 @@ const Signup = () => {
       <Meta title={"Sign Up"} />
       <BreadCrumb title="Sign Up" />
       <div className="login-wrapper home-wrapper-2 py-5">
-        <div className="row">
-          <div className="col-12">
-            <div className="auth-card">
-              <h3 className="text-center mb-3">SignUp</h3>
-              <form className="d-flex flex-column gap-15" action="">
-                <div className="mt-1">
-                  <input
-                    className="form-control"
-                    type="text"
-                    name="name"
-                    placeholder="Name"
-                  />
-                </div>
-                <div className="mt-1">
-                  <input
-                    className="form-control"
-                    type="email"
-                    name="email"
-                    placeholder="Email"
-                  />
-                </div>
-                <div className="mt-1">
-                  <input
-                    className="form-control"
-                    type="tel"
-                    name="mobile"
-                    placeholder="Mobile Number"
-                  />
-                </div>
-                <div className="mt-1">
-                  <input
-                    className="form-control"
-                    type="password"
-                    name="password"
-                    placeholder="Password"
-                  />
-                </div>
-                <div>
-                  <div className=" mt-3 d-flex justify-content-center align-items-center gap-15">
-                    <button className="button border-0">Sign Up</button>
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <div className="auth-card">
+                <h3 className="text-center mb-3">SignUp</h3>
+                <form className="d-flex flex-column gap-15" action="">
+                  <div className="mt-1">
+                    <input
+                      className="form-control"
+                      type="text"
+                      name="name"
+                      placeholder="Name"
+                    />
                   </div>
-                </div>
-              </form>
+                  <div className="mt-1">
+                    <input
+                      className="form-control"
+                      type="email"
+                      name="email"
+                      placeholder="Email"
+                    />
+                  </div>
+                  <div className="mt-1">
+                    <input
+                      className="form-control"
+                      type="tel"
+                      name="mobile"
+                      placeholder="Mobile Number"
+                    />
+                  </div>
+                  <div className="mt-1">
+                    <input
+                      className="form-control"
+                      type="password"
+                      name="password"
+                      placeholder="Password"
+                    />
+                  </div>
+                  <div>
+                    <div className=" mt-3 d-flex justify-content-center align-items-center gap-15">
+                      <button className="button border-0">Sign Up</button>
+                    </div>
+                  </div>
+                </form>
+              </div>
             </div>
           </div>
         </div>
