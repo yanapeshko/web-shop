6759730cecfbd99a0586458d616b66d67e85e375
Yana Peshko <yanapeshko917@gmail.com>
2023-05-03 09:46:50 -0400

    started Customer Review

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -17,6 +17,38 @@ const SingleProduct = () => {
           </div>
         </div>
       </div>
+      <div className="description-wrapper py-5 home-wrappper-2">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <div className="bg-white p-3">
+                <h4>Description</h4>
+                <p>
+                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+                  Ipsam repudiandae doloribus molestias possimus recusandae
+                  incidunt libero ipsum inventore perspiciatis eius. Assumenda
+                  amet impedit, error dolore mollitia dolores cumque. Labore,
+                  voluptas.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <section className="reviews-wrapper py-5 home-wrapper-2">
+        <div className="container-xxl">
+          <div className="row">
+            <div className="col-12">
+              <div className="review-head d-flex justify-content-between align-items-end">
+                <div>
+                  <h4>Customer Reviews</h4>
+                </div>
+                <div></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
       <section className="popular-wrapper py-5 home-wrapper-2">
         <div className="container-xxl">
           <div className="row">
@@ -25,9 +57,6 @@ const SingleProduct = () => {
             </div>
           </div>
           <div className="row">
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
             <ProductCard />
           </div>
         </div>
