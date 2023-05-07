6d320b7f8f5c986925482575292f6b91afc92823
Yana Peshko <yanapeshko917@gmail.com>
2023-05-07 12:40:32 -0400

    added reviews section

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
+import ReactStars from "react-rating-stars-component";
 
 import BreadCrumb from "../components/BreadCrumb";
 import Meta from "../components/Meta";
 import ProductCard from "../components/ProductCard";
 
 const SingleProduct = () => {
+  const [orederedProduct, setorederedProduct] = useState(true);
   return (
     <>
       <Meta title={"Product Name"} />
@@ -21,8 +23,8 @@ const SingleProduct = () => {
         <div className="container-xxl">
           <div className="row">
             <div className="col-12">
+              <h4>Description</h4>
               <div className="bg-white p-3">
-                <h4>Description</h4>
                 <p>
                   Lorem ipsum dolor sit, amet consectetur adipisicing elit.
                   Ipsam repudiandae doloribus molestias possimus recusandae
@@ -35,15 +37,85 @@ const SingleProduct = () => {
           </div>
         </div>
       </div>
-      <section className="reviews-wrapper py-5 home-wrapper-2">
+      <section className="reviews-wrapper home-wrapper-2">
         <div className="container-xxl">
           <div className="row">
             <div className="col-12">
-              <div className="review-head d-flex justify-content-between align-items-end">
-                <div>
-                  <h4>Customer Reviews</h4>
+              <h3>Reviews</h3>
+              <div className="review-inner-wrapper">
+                <div className="review-head d-flex justify-content-between align-items-end">
+                  <div>
+                    <h4 className="mb-2 "> Customer Reviews</h4>
+                    <div className="d-flex gap-10 align-items-center">
+                      <ReactStars
+                        count={5}
+                        value={4}
+                        edit={false}
+                        size={24}
+                        activeColor="#ffd700"
+                      />
+                      <p className="mb-0">Based on 2 Reviews</p>
+                    </div>
+                  </div>
+                  {orederedProduct && (
+                    <div>
+                      <a
+                        className="text-dark text-decoration-underline"
+                        href=""
+                      >
+                        Write a Review
+                      </a>
+                    </div>
+                  )}
                 </div>
-                <div></div>
+                <div className="review-form py-4">
+                  <h4> Write a Review</h4>
+                  <form action="" className="d-flex flex-column gap-15">
+                    <div>
+                      <ReactStars
+                        count={5}
+                        value={4}
+                        edit={true}
+                        size={24}
+                        activeColor="#ffd700"
+                      />
+                    </div>
+                    <div>
+                      <textarea
+                        name=""
+                        id=""
+                        className="w-100 form-control"
+                        cols="30"
+                        rows="4"
+                        placeholder="Comments"
+                      ></textarea>
+                    </div>
+                    <div className="d-flex justify-content-end">
+                      <button className="button border-0">Submit Review</button>
+                    </div>
+                  </form>
+                </div>
+                <div className="reviews mt-4">
+                  <div className="review">
+                    <div className="d-flex gap-10 align-items-center">
+                      <h6 className="mb-0">Navdeep</h6>
+                      <ReactStars
+                        count={5}
+                        value={4}
+                        edit={false}
+                        size={24}
+                        activeColor="#ffd700"
+                      />
+                    </div>
+                    <p className="mt-3">
+                      Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      Reiciendis excepturi quidem cum itaque ex ipsam quam
+                      quaerat commodi distinctio? Magnam consequuntur temporibus
+                      inventore nemo suscipit quae doloremque dolorum sequi
+                      minus?
+                    </p>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
