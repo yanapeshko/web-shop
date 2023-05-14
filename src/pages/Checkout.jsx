a7f9032e1b8dff2d653330ae984b7d5f089e174e
Yana Peshko <yanapeshko917@gmail.com>
2023-05-13 20:41:51 -0400

    added Shipping page

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { BiArrowBack } from "react-icons/bi";
+
+import watch from "../images/watch.jpg";
 
 const Checkout = () => {
   return (
@@ -15,18 +19,33 @@ const Checkout = () => {
                 >
                   <ol className="breadcrumb">
                     <li className="breadcrumb-item">
-                      <a href="#">Home</a>
+                      <Link className="text-dark total-price" to="/cart">
+                        Cart
+                      </Link>
                     </li>
                     &nbsp; /
-                    <li className="breadcrumb-item active" aria-current="page">
-                      Library
+                    <li
+                      className="breadcrumb-item active total-price"
+                      aria-current="page"
+                    >
+                      Infromation
+                    </li>
+                    &nbsp; /
+                    <li className="breadcrumb-item total-price">Shipping</li>
+                    &nbsp; /
+                    <li
+                      className="breadcrumb-item total-price"
+                      aria-current="page"
+                    >
+                      Payment
                     </li>
                   </ol>
                 </nav>
-                <h4 className="title">Contact Information</h4>
-                <p className="user-details">
+                <h4 className="title total">Contact Information</h4>
+                <p className="user-details total">
                   Yana Peshko (yanapeshko917@gmail.com)
                 </p>
+                <h4 className="mb-3">Shipping Address</h4>
                 <form
                   className="d-flex flex-wrap gap-15 justify-content-between"
                   action=""
@@ -87,10 +106,60 @@ const Checkout = () => {
                       placeholder="ZIP code"
                     />
                   </div>
+                  <div className="w-100">
+                    <div className="d-flex justify-content-between align-items-center">
+                      <Link className="text-dark" to="/cart">
+                        <BiArrowBack className="me-2" />
+                        Return to Cart
+                      </Link>
+                      <Link className="button" to="/cart">
+                        Continue to Shipping
+                      </Link>
+                    </div>
+                  </div>
                 </form>
               </div>
             </div>
-            <div className="col-5"></div>
+            <div className="col-5">
+              <div className="border-bottom py-4">
+                <div className="d-flex gap-10 mb-2 align-items-center">
+                  <div className="w-75 d-flex gap-10">
+                    <div className="w-25 position relative">
+                      <span
+                        style={{ top: "-10px", right: "530px" }}
+                        className="badge bg-secondary text-white rounded-circle p-2 position-absolute"
+                      >
+                        1
+                      </span>
+                      <img className="img-fluid" src={watch} alt="product" />
+                    </div>
+                    <div>
+                      <h5 className="total-price">Title</h5>
+                      <p className="total-price">s / paragraph</p>
+                    </div>
+                  </div>
+
+                  <div className="flex-grow-1">
+                    <h5 className="total">$ 100</h5>
+                  </div>
+                </div>
+              </div>
+              <div className="border-bottom py-4">
+                <div className="d-flex justify-content-between align-items-center">
+                  <p className="total">Subtotal</p>
+                  <p className="total-price">$ 1000</p>
+                </div>
+
+                <div className="d-flex justify-content-between align-items-center">
+                  <p className="mb-0 total">Shipping</p>
+                  <p className="mb-0 total-price">$ 1000</p>
+                </div>
+              </div>
+              <div className="d-flex justify-content-between align-items-center border-bottom py-4">
+                <h4 className="total">Total</h4>
+                <h5 className="total-price">$ 1000</h5>
+              </div>
+            </div>
           </div>
         </div>
       </div>
